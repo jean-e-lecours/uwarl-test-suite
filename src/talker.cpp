43e19de47b15c2da5af9f98aa32f63b146936154
jean-e-lecours <jlecours@uwaterloo.ca>
2022-06-24 14:27:22 -0400

    Started working on it

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,9 +1,14 @@
 #include <chrono>
+#include <memory>
 #include <functional>
+#include <geometry_msgs/msg/detail/pose_stamped__struct.hpp>
 #include <memory>
 #include <string>
 
+
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/PoseStamped.h"
+#include "gazebo_msgs/ModelState.h"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
@@ -11,20 +16,21 @@ using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class ASMPublisher : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
+    ASMPublisher()
+    : Node("asm_publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("asm_pos", 10);
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&ASMPublisher::asm_callback, this));
     }
 
   private:
-    void timer_callback()
+    void asm_callback()
     {
+      // ASM function goes here
       auto message = std_msgs::msg::String();
       message.data = "Hello, world! " + std::to_string(count_++);
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
@@ -35,10 +41,29 @@ class MinimalPublisher : public rclcpp::Node
     size_t count_;
 };
 
+class GzSubscriber : public rclcpp::Node
+{
+  public:
+    GzSubscriber()
+    : Node("gz_subscriber")
+    {
+      subscription_ = this->create_subscription<gazebo_msgs::msg::ModelState>(
+      "gazebo/model_states", 10, std::bind(&GzSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<ASMPublisher>());
   rclcpp::shutdown();
   return 0;
 }
